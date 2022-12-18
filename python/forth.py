b5481870b25daaaad526622edd32e6cae3cdcb79
yemyintko <yemyintko2024.ru@gamil.com>
2022-12-18 12:43:09 +0630

    trafffic light

---
+++ python/forth.py
@@ -0,0 +1,10 @@
+seconds = 50
+while seconds >0:
+    if seconds ==50:
+        print("Go!")
+    elif seconds ==20:
+        print("Wait!")
+    elif seconds ==10:
+        print("Stop!")
+
+    seconds = seconds -1
